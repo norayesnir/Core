c9d1feeac528ed150310b8488f0e01a8ef2acafb
Rinse Yaron Schaeffer <rinseyaron@Rinses-MBP-2.owner.local>
2024-06-12 15:44:02 +0200

    🚀 Init

---
+++ apps/server/src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import payload from 'payload'
+
+require('dotenv').config()
+const app = express()
+
+// Redirect root to Admin panel
+app.get('/', (_, res) => {
+  res.redirect('/admin')
+})
+
+const start = async () => {
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET,
+    express: app,
+    onInit: async () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+      payload.logger.info(`Payload API URL: ${payload.getAPIURL()}`)
+      payload.logger.info(`Payload Public Port: ${process.env.PAYLOAD_PUBLIC_PORT}`)
+    },
+  })
+
+  app.listen(process.env.PAYLOAD_PUBLIC_PORT)
+}
+
+start()
